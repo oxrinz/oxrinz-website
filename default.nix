831a5d2525a800494e3528a46cbb1ab6e2df561a
rinz <roberts.denisovs@gmail.com>
2025-02-06 07:14:30 +0200

    updated shell to include nixpkgs tarball

--- default.nix
+++ default.nix
@@ -1,3 +1,12 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-pkgs.mkShell { buildInputs = with pkgs; [ nodejs nodePackages.npm ]; }
+let
+  nixpkgs = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz";
+  };
+  pkgs = import nixpkgs {};
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    nodejs_20  
+    nodePackages.npm
+  ];
+}
