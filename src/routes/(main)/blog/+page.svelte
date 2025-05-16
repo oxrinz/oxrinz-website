1cf94370a9eb166a23822cbb61be8f66b4311109
rinz <roberts.denisovs@gmail.com>
2025-05-16 21:51:04 +0200

    new bloggit push

--- src/routes/(main)/blog/+page.svelte
+++ src/routes/(main)/blog/+page.svelte
@@ -1,6 +1,7 @@
 <div class="col-span-2 flex flex-col gap-8">
   <h1>Blog</h1>
   <div class="flex flex-col">
+    <a href="/blog/a-month-later">A month later (RyuLang) (16.05.2025)</a>
     <a href="/blog/ryuthon">RyuLang (14.04.2025)</a>
   </div>  
 
