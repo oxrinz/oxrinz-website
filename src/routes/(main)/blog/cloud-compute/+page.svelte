f6b951638e566c3d544414e1f9009bb6bf171547
oxrinz <roberts.denisovs@gmail.com>
2025-08-10 04:57:47 +0300

    cloud compute

---
+++ src/routes/(main)/blog/cloud-compute/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+  import Divider from "$lib/components/divider.svelte";
+</script>
+
+<h1>cloud compute</h1>
+<p>
+  State of cloud compute is bad, especially if you're a person like me. I'm a
+  tinkerer, I never train models, I don't think I've ever used a gpu to its full
+  capacity, other than maybe 1 or 2 times. All I care about is getting an
+  interface to a gpu of whatever architecture, throwing sticks at it and seeing
+  how it responds. Other users can use that gpu as much as they want for all I
+  care, I'm not efficiencymaxxing, I'm simply developing software for this gpu.
+  5 copies of oxrinz could work on the same machine without noticing each
+  others' existence.
+</p>
+<br />
+<br />
+<p>
+  Unless you have your own computer, for some reason, this is ... hard to do to
+  say the least. Most cloud compute providers expect you to use them at max
+  capacity, therefore charging an enormous amount for the worth of what I'm
+  trying to do. I don't care about calculating costs, I don't care about
+  checking up on my balance every hour, I don't care about uptime, reliability,
+  most of the time I wouldn't even care about bottlenecked hardware lol
+</p>
+<br />
+<br />
+<p>
+  In an ideal world, I'd pay x a month to access all kinds of gpus on certain
+  terms. You can set me a usage limit, I probably won't hit it. The machines
+  should feel as though they're mine, fast startup times, the things I need
+  should be cached in one way or another, and I should be able to use my nix
+  flakes on all of them. I want to be able to do a quick 20 minute coding
+  sessions without spending half of the time on setting up an instance.
+</p>
+<br />
+<br />
+<p>
+  I've been trying to get access to a tenstorrent machine for the past ~6 months
+  with 0 luck, koyeb doesn't want to open their compute for me, and there is no
+  tenstorrent on runpod, or any other cloud compute provider. I literally got
+  bullied into buying a card just because no one has it up for rent. I don't
+  understand why this is the case, there are so many hpc devs seeking a better
+  life, why is there no gpu cloud provider built for this fact.
+</p>
+<br />
+<br />
+<p>
+  I'm not interested in making a startup but I am interested in solving this
+  problem, at the very least for myself. To be precise, I'm looking for cloud
+  compute that's 10x cheaper, and has 10x faster startup times. I don't know how
+  I'll do it, but I know it's possible. Once I figure out the solution, I'll
+  post this blog and give you access to it.
+</p>
